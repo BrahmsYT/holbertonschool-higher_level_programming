9daa445b071a5e7adfda7cabf4ab5b85ce51fe3f
BrahmsYT <qasimlirenat@gmail.com>
2025-12-02 00:08:59 +0000

    sdkskd

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        if len(sentence) == 0:
+    if len(sentence) == 0:
         first_char = None
     else:
         first_char = sentence[0]
